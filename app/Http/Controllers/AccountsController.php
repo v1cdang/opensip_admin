91cd15c682f7eabae643d2b8568a3b7e9af1a2e3
Vic <adjolin@gmail.com>
2020-07-11 15:01:42 +0800

    Initial commit

---
+++ app/Http/Controllers/AccountsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AccountsController extends Controller
+{
+    public function index()
+    {
+        $prefixes = DB::table('cc_card')->select('prefix')->get();
+        return view('addCredit', ['prefixes' => $prefixes]);
+    }
+    public function addCreditForm($prefix)
+    {
+        $prefixes = DB::table('cc_card')->select('prefix')->get();
+        return view('addCreditForm', ['selectedPrefix' => $prefix, 'prefixes' => $prefixes]);
+    }
+    public function updateCredit(Request $request)
+    {
+        DB::enableQueryLog();
+        $selectedPrefix = $request->input('selectedPrefix');
+        $credit_amount =  (int) $request->input('credit_amount');
+
+        $previous_balance = DB::table('cc_card')->select('credit')->where('prefix', $selectedPrefix)->first();
+
+        $current_balance = $previous_balance->credit + $credit_amount;
+        DB::table('cc_card')->where('prefix', $selectedPrefix)
+                           ->increment('credit',$credit_amount);
+        DB::table('account_credit_history')->insert(
+            [
+                'prefix' => $selectedPrefix,
+                'previous_balance' => $previous_balance->credit,
+                'credit_amount' => $credit_amount,
+                'current_balance' => $current_balance
+            ]
+        );
+    }
+}
