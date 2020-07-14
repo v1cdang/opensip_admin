1d7797675afa19eedb5a1ee0fb09ccf430851df7
Vic <adjolin@gmail.com>
2020-07-14 11:03:13 +0800

    callerid changes

--- app/Http/Controllers/CalleridController.php
+++ app/Http/Controllers/CalleridController.php
@@ -7,9 +7,30 @@ use Illuminate\Support\Facades\DB;
 
 class CalleridController extends Controller
 {
-    public function index($prefix, $ipaddress)
+    public function index($prefix, $phonenumber, $ipaddress)
     {
-        $callerids = DB::table('callerids')->select('callerid')->where(['prefix'=>$prefix, 'ip'=>$ipaddress])->inRandomOrder()->get();
-        echo $callerids[0]->callerid;
+
+        $callerid = '';
+        if ($prefix=='3300') {
+            $available_callerids['CA'] = '13236419245';
+            $available_callerids['CO'] = '13034810082';
+            $available_callerids['CT'] = '18604125025';
+            $available_callerids['FL'] = '19042484331';
+            $available_callerids['IL'] = '13128210893';
+            $available_callerids['MA'] = '16175318167';
+            $available_callerids['NJ'] = '19738491638';
+            $available_callerids['NM'] = '15053539918';
+            $available_callerids['NV'] = '17252060624';
+            $available_callerids['TX'] = '19723672084';
+            $available_callerids['UT'] = '18016566113';
+            $npanxx = substr($phonenumber, 1, 6);
+            $state = DB::table('npanxx')->select('stateISO')->where(['npanxx'=>$npanxx])->first();
+            $callerid = $available_callerids[$state->stateISO];
+            if ($callerid!='')
+            echo $callerid;
+        } else {
+            $callerids = DB::table('callerids')->select('callerid')->where(['prefix'=>$prefix, 'ip'=>$ipaddress])->inRandomOrder()->get();
+            echo $callerids[0]->callerid;
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('addCredit/{prefix}', 'AccountsController@addCreditForm');
 Route::post('addCreditforPrefix', 'AccountsController@updateCredit');
 Route::get('creditHistory', 'CreditsController@index');
 Route::get('creditHistory/{prefix}', 'CreditsController@viewPrefixCreditHistory');
-Route::get('getCallerIds/{prefix}/{ipaddress}', 'CalleridController@index');
+Route::get('getCallerIds/{prefix}/{phonenumber}/{ipaddress}', 'CalleridController@index');
 
 Auth::routes();
 
