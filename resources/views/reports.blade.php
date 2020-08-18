b8a743233afc86d8d158ff0461c2908300e1c7e3
Vic <adjolin@gmail.com>
2020-08-18 23:45:38 +0800

    reports

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -7,7 +7,7 @@
             <table>
                 <tr><td>PREFIX</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
             @foreach ($okCalls as $okCall):
-            <tr><td>{{ dd($okCall) }}</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
+            <tr><td>{{ $okCall->PREFIX }}</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
             @endforeach
             </table>
     </div>
