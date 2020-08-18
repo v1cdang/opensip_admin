1a56b008c729f2b6d04725b41ca4ec4267b7f829
Vic <adjolin@gmail.com>
2020-08-18 23:55:39 +0800

    reports

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -6,9 +6,10 @@
         View Current Summary
             <table>
                 <tr><td>PREFIX</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
-            @foreach ($okCalls as $okCall):
-            <tr><td>{{ $okCall->PREFIX }}</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
-            @endforeach
+                @foreach ($okCalls as $okCall)
+                <tr><td>{{ $okCall->PREFIX }}</td><td>{{ $okCall->TOTAL}}</td><td>{{ $okCall->DURATION }}</td><td>{{ $okCall->customerCallCost }}</td>
+                <td>{{ $okCall->avgcustomerCost }}</td><td>{{ $okCall->vendorCallCost }}</td><td>{{ $okCall->avgcarrierCost }}</td><td>{{ $okCall->carrierid }}</td></tr>
+                @endforeach
             </table>
     </div>
 @endsection
