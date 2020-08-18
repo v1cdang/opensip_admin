f80138f4ade7cf03ed844f3aaaf12bea913c2f20
Vic <adjolin@gmail.com>
2020-08-18 22:47:05 +0800

    reports

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -4,9 +4,11 @@
  @section('content')
     <div>
         View Current Summary
-            @foreach ($prefixes as $prefix):
-            <option value="creditHistory/{{ $prefix->prefix }}">{{ $prefix->prefix }} </option>
+            <table>
+                <tr><td>PREFIX</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
+            @foreach ($okCalls as $okCall):
+            <tr><td>{{ $okCall->prefix }}</td><td>TOTAL</td><td>DURATION</td><td>Customer Total Cost</td><td>Average Customer Rate</td><td>Total Vendor Call Cost</td><td>Average Carrier Rate</td><td>carrierid</td></tr>
             @endforeach
-
+            </table>
     </div>
 @endsection
