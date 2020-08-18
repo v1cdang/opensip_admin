152d8e8417263483ca6606f4431d9a2bd6f7d83f
Vic <adjolin@gmail.com>
2020-08-19 04:54:50 +0800

    reeports

--- resources/views/reports.blade.php
+++ resources/views/reports.blade.php
@@ -5,8 +5,9 @@
     <div>
         View Current Summary
             <table>
+                <tr><td colspan="5"><h3>Start time for this query is: {{ $okCalls[0]->date_start }}</h3></td></tr>
                 <tr><td>PREFIX</td><td>TOTAL</td><td>carrierid</td><td>SIP Code</td><td>Sip Reason</td></tr>
-            <tr><td colspan="5">Start time for this query is: {{ $okCalls[0]->date_start }}</td></tr>
+
                 @foreach ($okCalls as $okCall)
                 <tr><td>{{ $okCall->PREFIX }}</td><td>{{ $okCall->TOTAL}}</td><td>{{ $okCall->carrierid }}</td><td>{{ $okCall->sip_code }}</td><td>{{ $okCall->sip_reason }}</td></tr>
                 @endforeach
