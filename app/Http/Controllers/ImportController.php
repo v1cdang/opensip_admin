81af441b40493e9986818554860158da2239d0e9
Vic <adjolin@gmail.com>
2020-09-10 03:44:35 +0800

    csv import

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Requests\CsvImportRequest;
 use App\CsvData;
 use App\DawzCDRs;
@@ -18,18 +19,15 @@ class ImportController extends Controller
     {
         $path = $request->file('csv_file')->getRealPath();
         $data = array_map('str_getcsv', file($path));
-        $csv_data_file = CsvData::create([
-            'csv_filename' => $request->file('csv_file')->getClientOriginalName(),
-            'csv_header' => $request->has('header'),
-            'csv_data' => json_encode($data)
-        ]);
-
-        $csv_data = array_slice($data, 0, 2);
-        return view('import_fields', compact('csv_data', 'csv_data_file'));
+        $query = "LOAD DATA LOCAL INFILE '$path'
+        INTO TABLE dawz_cdr FIELDS TERMINATED BY ','
+        LINES TERMINATED BY '\r\n'
+        IGNORE 1 LINES;";
     }
 
     public function processImport(Request $request)
     {
+        DB::connection()->disableQueryLog();
         $data = CsvData::find($request->csv_data_file_id);
         $csv_data = json_decode($data->csv_data, true);
         $db_fields = config('app.db_fields');
