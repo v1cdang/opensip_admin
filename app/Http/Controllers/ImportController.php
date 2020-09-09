66d57eba3724a898948635f5df5619fb7375c718
Vic <adjolin@gmail.com>
2020-09-10 05:25:35 +0800

    import csv

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -29,7 +29,7 @@ class ImportController extends Controller
         $mimeType = $file->getMimeType();
         //$path = Storage::putFile('/var/lib/mysql-files/'.$filename.".".$extension, $request->file('csv_file'));
         rename($tempPath, '/var/lib/mysql-files/'.$filename);
-        $query = "LOAD DATA INFILE '$filename'
+        $query = "LOAD DATA INFILE '/var/lib/mysql-files/$filename'
         INTO TABLE dawz_cdr
         FIELDS TERMINATED BY ','
         LINES TERMINATED BY '\n';";
